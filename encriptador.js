6fc5acb6883548fa09051c540b0829db1ba8edea
Jhonayo <j.iramirezpavez@gmail.com>
2023-05-16 15:20:12 -0400

    creacion de archivo js con funciones

---
+++ encriptador.js
@@ -0,0 +1,20 @@
+const textArea = document.querySelector(".text_area");
+const mensaje = document.querySelector(".mensaje");
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+}
+
+function encriptar(stringEncriptado){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for(let i=0; i< matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+
+        }
+    }
+    return stringEncriptado
+}
